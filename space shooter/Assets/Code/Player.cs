5217000a34b24973a3d1652ae65399b9447a94da
algoritmika14 <algomklass@gmail.com>
2023-03-05 14:22:11 +0500

    sum2

---
+++ space shooter/Assets/Code/Player.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    void Update()
+    {
+        Move();
+    }
+
+    // Start is called before the first frame update
+    void Move()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            Vector2 mousePos = Input.mousePosition;
+            Vector2 realPos = Camera.main.ScreenToWorldPoint(mousePos);
+            transform.position = realPos;
+        }
+    
+       
+    }
+
+    // Update is called once per frame
+    
+}
